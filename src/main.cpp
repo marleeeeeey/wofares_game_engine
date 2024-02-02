f8ac4a16f424781be33f1fd90c74bb94ec623788
marleeeeeey <marleeeeeey@gmail.com>
2024-02-03 01:48:47 +0400

    Build with EnTT+SDL+gml libs. Gravity and key control.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <SDL.h>
+#include <entt/entt.hpp>
 #include <glm/glm.hpp>
 #include <iostream>
 
-int main( int argc, char* argv[] )
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const float GRAVITY = 0.5f;
+const float JUMP_VELOCITY = -15.0f;
+const float MOVE_SPEED = 5.0f;
+
+struct Position
+{
+    glm::vec2 value;
+};
+
+struct Velocity
+{
+    glm::vec2 value;
+};
+
+int main( int argc, char* args[] )
 {
     if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
@@ -11,24 +28,17 @@ int main( int argc, char* argv[] )
     }
 
     SDL_Window* window = SDL_CreateWindow(
-        "Bouncing Ball", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN );
-    if ( !window )
-    {
-        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        return -1;
-    }
-
+        "Bouncing Ball with SDL, EnTT & GLM", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH,
+        WINDOW_HEIGHT, SDL_WINDOW_SHOWN );
     SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
-    if ( !renderer )
-    {
-        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        return -1;
-    }
+
+    entt::registry registry;
+    auto ball = registry.create();
+    registry.emplace<Position>( ball, glm::vec2( WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 ) );
+    registry.emplace<Velocity>( ball, glm::vec2( 0, 0 ) );
 
     bool quit = false;
     SDL_Event e;
-    glm::vec2 ballPos( 400, 300 );
-    glm::vec2 ballVel( 0, -5 );
 
     while ( !quit )
     {
@@ -40,20 +50,42 @@ int main( int argc, char* argv[] )
             }
         }
 
-        ballPos += ballVel;
-        ballVel.y += 0.2f; // Gravity effect
-
-        // Bounce off the floor and ceiling
-        if ( ballPos.y >= 600 || ballPos.y <= 0 )
+        const Uint8* currentKeyStates = SDL_GetKeyboardState( NULL );
+        if ( currentKeyStates[SDL_SCANCODE_UP] )
         {
-            ballVel.y = -ballVel.y;
+            registry.get<Velocity>( ball ).value.y = JUMP_VELOCITY;
+        }
+        if ( currentKeyStates[SDL_SCANCODE_LEFT] )
+        {
+            registry.get<Velocity>( ball ).value.x = -MOVE_SPEED;
+        }
+        else if ( currentKeyStates[SDL_SCANCODE_RIGHT] )
+        {
+            registry.get<Velocity>( ball ).value.x = MOVE_SPEED;
+        }
+        else
+        {
+            registry.get<Velocity>( ball ).value.x = 0;
+        }
+
+        // Update physics
+        auto& pos = registry.get<Position>( ball );
+        auto& vel = registry.get<Velocity>( ball );
+
+        vel.value.y += GRAVITY; // Apply gravity
+        pos.value += vel.value; // Update position
+
+        // Collision with ground
+        if ( pos.value.y > WINDOW_HEIGHT - 50 )
+        {
+            pos.value.y = WINDOW_HEIGHT - 50;
+            vel.value.y = 0;
         }
 
         SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
         SDL_RenderClear( renderer );
 
-        // Draw the ball
-        SDL_Rect ballRect = { static_cast<int>( ballPos.x - 10 ), static_cast<int>( ballPos.y - 10 ), 20, 20 };
+        SDL_Rect ballRect = { static_cast<int>( pos.value.x ) - 25, static_cast<int>( pos.value.y ) - 25, 50, 50 };
         SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
         SDL_RenderFillRect( renderer, &ballRect );
 
