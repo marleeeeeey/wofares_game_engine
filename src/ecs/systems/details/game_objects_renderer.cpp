27dc8ec547d4a03cef0c049e3fbc10f41b4bcc66
marleeeeeey <marleeeeeey@gmail.com>
2024-02-15 13:18:43 +0400

    Refactoring GameObjectsRenderer.

--- src/ecs/systems/details/game_objects_renderer.cpp
+++ src/ecs/systems/details/game_objects_renderer.cpp
@@ -21,35 +21,34 @@ void GameObjectsRenderer::RenderTiles()
     for (auto entity : tilesView)
     {
         const auto& [size, tileInfo, physicalBody] = tilesView.get<SizeComponent, TileInfo, PhysicalBody>(entity);
-        b2Vec2 pos = physicalBody.value->GetBody()->GetPosition() * box2DtoSDL;
-        float angle = physicalBody.value->GetBody()->GetAngle();
+        RenderTiledSquare(physicalBody.value, size.value, tileInfo);
+    }
+}
 
-        // Compute the destination rectangle on the screen.
-        glm::vec2 transformedPosition =
-            (pos - gameState.renderingOptions.cameraCenter) * gameState.renderingOptions.cameraScale +
-            gameState.renderingOptions.windowSize / 2.0f;
+void GameObjectsRenderer::RenderPlayers()
+{
+    auto players = registry.view<PhysicalBody, SizeComponent, PlayerNumber>();
+    for (auto entity : players)
+    {
+        const auto& [physicalBody, size] = players.get<PhysicalBody, SizeComponent>(entity);
+        RenderSquare(physicalBody.value, size.value, ColorName::Blue);
+    }
+}
 
-        // Have to render from the center of the object. Because the Box2D body is in the center of the object.
-        SDL_Rect destRect = {
-            static_cast<int>(transformedPosition.x - size.value.x * gameState.renderingOptions.cameraScale / 2),
-            static_cast<int>(transformedPosition.y - size.value.y * gameState.renderingOptions.cameraScale / 2),
-            static_cast<int>(size.value.x * gameState.renderingOptions.cameraScale),
-            static_cast<int>(size.value.y * gameState.renderingOptions.cameraScale)};
-
-        // Calculate the angle in degrees.
-        SDL_Point center = {destRect.w / 2, destRect.h / 2};
-        double angleDegrees = angle * 180.0 / std::numbers::pi;
-
-        // Render the tile with the calculated angle.
-        SDL_RenderCopyEx(
-            renderer, tileInfo.texture->get(), &tileInfo.srcRect, &destRect, angleDegrees, &center, SDL_FLIP_NONE);
+void GameObjectsRenderer::RenderGranades()
+{
+    auto granades = registry.view<PhysicalBody, SizeComponent, Granade>();
+    for (auto entity : granades)
+    {
+        const auto& [physicalBody, size] = granades.get<PhysicalBody, SizeComponent>(entity);
+        RenderSquare(physicalBody.value, size.value, ColorName::Red);
     }
 }
 
 void GameObjectsRenderer::RenderSquare(std::shared_ptr<Box2dObjectRAII> body, const glm::vec2& sdlSize, ColorName color)
 {
     const b2Vec2 pos = body->GetBody()->GetPosition() * box2DtoSDL;
-    // TODO implement texture and angle for players.
+    // TODO implement texture and angle support for non tiled objects.
     const float angle = body->GetBody()->GetAngle();
 
     SetRenderDrawColor(renderer, color);
@@ -68,22 +67,29 @@ void GameObjectsRenderer::RenderSquare(std::shared_ptr<Box2dObjectRAII> body, co
     SDL_RenderFillRect(renderer, &rect);
 }
 
-void GameObjectsRenderer::RenderPlayers()
+void GameObjectsRenderer::RenderTiledSquare(
+    std::shared_ptr<Box2dObjectRAII> body, const glm::vec2& sdlSize, const TileInfo& tileInfo)
 {
-    auto players = registry.view<PhysicalBody, SizeComponent, PlayerNumber>();
-    for (auto entity : players)
-    {
-        const auto& [physicalBody, size] = players.get<PhysicalBody, SizeComponent>(entity);
-        RenderSquare(physicalBody.value, size.value, ColorName::Blue);
-    }
-}
+    const b2Vec2 sdlPos = body->GetBody()->GetPosition() * box2DtoSDL;
+    const float angle = body->GetBody()->GetAngle();
 
-void GameObjectsRenderer::RenderGranades()
-{
-    auto granades = registry.view<PhysicalBody, SizeComponent, Granade>();
-    for (auto entity : granades)
-    {
-        const auto& [physicalBody, size, granade] = granades.get<PhysicalBody, SizeComponent, Granade>(entity);
-        RenderSquare(physicalBody.value, size.value, ColorName::Red);
-    }
+    // Compute the destination rectangle on the screen.
+    glm::vec2 transformedPosition =
+        (sdlPos - gameState.renderingOptions.cameraCenter) * gameState.renderingOptions.cameraScale +
+        gameState.renderingOptions.windowSize / 2.0f;
+
+    // Have to render from the center of the object. Because the Box2D body is in the center of the object.
+    SDL_Rect destRect = {
+        static_cast<int>(transformedPosition.x - sdlSize.x * gameState.renderingOptions.cameraScale / 2),
+        static_cast<int>(transformedPosition.y - sdlSize.y * gameState.renderingOptions.cameraScale / 2),
+        static_cast<int>(sdlSize.x * gameState.renderingOptions.cameraScale),
+        static_cast<int>(sdlSize.y * gameState.renderingOptions.cameraScale)};
+
+    // Calculate the angle in degrees.
+    SDL_Point center = {destRect.w / 2, destRect.h / 2};
+    double angleDegrees = angle * 180.0 / std::numbers::pi;
+
+    // Render the tile with the calculated angle.
+    SDL_RenderCopyEx(
+        renderer, tileInfo.texture->get(), &tileInfo.srcRect, &destRect, angleDegrees, &center, SDL_FLIP_NONE);
 }
--- src/ecs/systems/details/game_objects_renderer.h
+++ src/ecs/systems/details/game_objects_renderer.h
@@ -11,9 +11,11 @@ class GameObjectsRenderer
     GameState& gameState;
 public:
     GameObjectsRenderer(entt::registry& registry, SDL_Renderer* renderer);
-private:
+private: // Render game objects methods.
     void RenderTiles();
     void RenderPlayers();
     void RenderGranades();
+private: // Render helpers.
     void RenderSquare(std::shared_ptr<Box2dObjectRAII> body, const glm::vec2& sdlSize, ColorName color);
+    void RenderTiledSquare(std::shared_ptr<Box2dObjectRAII> body, const glm::vec2& sdlSize, const TileInfo& tileInfo);
 };
